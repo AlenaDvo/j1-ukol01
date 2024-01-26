0723244aa335ef2e0068383cfed4c94f60e32a1f
Alena Dvorakova <alenadvorakova@technicky-preklad.cz>
2024-01-26 12:58:21 +0100

    Nakresleno prase.

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -3,16 +3,110 @@ package cz.czechitas.java.ukol01;
 import cz.czechitas.java.ukol01.engine.Turtle;
 
 public class HlavniProgram {
-  private Turtle zofka;
+    private Turtle zofka;
 
-  public static void main(String[] args) {
-    new HlavniProgram().start();
-  }
+    public static void main(String[] args) {
+        new HlavniProgram().start();
+    }
 
-  public void start() {
-    zofka = new Turtle();
+    public void start() {
+        zofka = new Turtle();
 
-    //TODO implementace domácího úkolu
-  }
+        nakresliPrasatko(100);
+    }
 
+    private void nakresliPrasatko(int strana) {
+        nakresliObdelnik(strana);
+
+        // predni nohy
+        nakresliNohy(strana);
+
+        // presun a zadni nohy
+        zofka.turnRight(90);
+        zofka.move(strana * 1.5);
+        zofka.turnLeft(90);
+        nakresliNohy(strana);
+
+        // presun, hlava a oko
+        zofka.turnLeft(90);
+        zofka.move(strana * 1.5);
+        nakresliHlavu(strana);
+        zofka.turnRight(120);
+        zofka.move(strana / 2);
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(strana / 2);
+        zofka.penDown();
+        zofka.move(1);
+        zofka.turnRight(180);
+        zofka.penUp();
+        zofka.move(strana / 2);
+        zofka.turnLeft(90);
+        zofka.move(strana / 2);
+
+        // ocas
+        zofka.turnRight(90);
+        zofka.move(strana * 1.5);
+        zofka.turnRight(45);
+
+        zofka.penDown();
+        for (int i = 0; i < 10; i++) {
+            zofka.move(10);
+            zofka.turnLeft(180 / 10);
+        }
+        for (int i = 0; i < 10; i++) {
+            zofka.move(7);
+            zofka.turnLeft(180 / 10);
+        }
+        for (int i = 0; i < 10; i++) {
+            zofka.move(3);
+            zofka.turnLeft(180 / 10);
+        }
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(strana / 2);
+        zofka.turnLeft(45);
+        zofka.move(strana);
+        zofka.turnRight(180);
+    }
+
+    private void nakresliObdelnik(int strana) {
+        zofka.penDown();
+        for (int i = 0; i < 2; i++) {
+            zofka.move(strana);
+            zofka.turnRight(90);
+            zofka.move(strana * 1.5);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
+    }
+
+    private void nakresliNohy(int strana) {
+//        1. noha
+        zofka.turnLeft(150);
+        zofka.penDown();
+        zofka.move(strana / 2);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(strana / 2);
+        zofka.turnRight(150);
+
+//        2. noha
+        zofka.turnLeft(30);
+        zofka.penDown();
+        zofka.move(strana / 2);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(strana / 2);
+        zofka.turnRight(30);
+    }
+
+    private void nakresliHlavu(int strana) {
+        zofka.penDown();
+        zofka.turnRight(30);
+        zofka.move(strana);
+        zofka.turnRight(120);
+        zofka.move(strana);
+        zofka.penUp();
+    }
 }
